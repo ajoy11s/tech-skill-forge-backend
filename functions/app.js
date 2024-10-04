79822c5747829b151dbcff620a7488a13c8abaec
ajoy11s <ajoycse0811@gmail.com>
2024-10-05 00:27:04 +0600

    changes

--- functions/app.js
+++ functions/app.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const port = process.env.port || 3000;
 
-const products = require('./data/courseDetails.json');
+const products = require('/courseDetails.json');
 
 
 app.use(cors());
