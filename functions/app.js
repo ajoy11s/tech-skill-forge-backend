f705c46baad42d927a1525df35b477a75f7a8d5b
ajoy11s <ajoycse0811@gmail.com>
2024-10-04 23:35:52 +0600

    changes

---
+++ functions/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
+
+const port = process.env.port || 3000;
+
+const products = require('./data/courseDetails.json');
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/products', (req, res) => {
+  res.send(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  console.log("11:"+req.params.id);
+  const productIdByProducts = products.filter((n) => n._id === id);
+
+  if (productIdByProducts.length > 0) {
+    res.send(productIdByProducts);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+});
+
+
+// app.listen(port, () => {
+//   console.log(`Hello world ${port}`);
+// });
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
